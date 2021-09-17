1a7feada0bb316e29e770e5ccdfae6af8f0e23fa
octo <dlqhdgkr456@gmail.com>
2021-09-17 13:40:05 +0900

    1.4 Recap Refactor

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Vanilla Redux
 
-Learning Vanilla-Redux
+Learning Vanilla-Redux  
+
+## Learned
+
+reducer : 현재 상태의 application과 함께 불려지는 function (+ with action), return하는 것은 application의 state가 됨  
+action : reducer와 소통하는 방법으로 Object여야 하며 그 key 이름은 항상 type임 (바꿀 수 없음)  
+dispatch : reducer에게 action을 보내는 방법  
+subscribe : store의 변화를 감지하면 인자값으로 준 함수를 실행  
+  
+switch가 자주 쓰임  
+```
+switch(action.type){
+  case ..blah..:
+    return something;
+  case ..blah2..:
+    return something2;
+  default:
+    return something3;
+}
+```
+  
+string으로 바로 쓰는 대신에 const variable로 선언해서 사용하기 -> 에러 발견 용이
--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ const plus = document.querySelector("#plus");
 const minus = document.querySelector("#minus");
 const number = document.querySelector("span");
 
+const PLUS = "PLUS";
+const MINUS = "MINUS";
+
 // reducer는 data를 modify 해주는 함수로 reducer가 return하는 것은 application에 있는 data가 됨
 // action : redux에서 function을 부를 때 쓰는 두 번째 parameter 혹은 argument으로 reducer와 소통하기 위한 방법
 const reducer = (state = 0, action) => {
-  if (action.type === "PLUS") {
-    return state + 1;
-  } else if (action.type === "MINUS") {
-    return state - 1;
-  } else {
-    return state;
+  switch (action.type ) {
+    case PLUS:
+      return state + 1;
+    case MINUS:
+      return state - 1;
+    default:
+      return state;
   }
 };
 
@@ -29,5 +33,5 @@ const onChange = () => {
 store.subscribe(onChange);
 
 // reducer에게 Action을 보내는 방법 : store.dispatch({key: value});
-plus.addEventListener("click", () => store.dispatch({ type: "PLUS"}));
-minus.addEventListener("click", () => store.dispatch({ type: "MINUS"}));
+plus.addEventListener("click", () => store.dispatch({ type: PLUS}));
+minus.addEventListener("click", () => store.dispatch({ type: MINUS}));
