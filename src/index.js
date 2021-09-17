92722e516a688225b5b0140196447e491d841f31
octo <dlqhdgkr456@gmail.com>
2021-09-17 13:19:52 +0900

    1.3 Subscriptions

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ const number = document.querySelector("span");
 // reducer는 data를 modify 해주는 함수로 reducer가 return하는 것은 application에 있는 data가 됨
 // action : redux에서 function을 부를 때 쓰는 두 번째 parameter 혹은 argument으로 reducer와 소통하기 위한 방법
 const reducer = (state = 0, action) => {
-  console.log(state, action);
-  if (action.type === "ADD") {
+  if (action.type === "PLUS") {
     return state + 1;
   } else if (action.type === "MINUS") {
     return state - 1;
@@ -17,18 +16,18 @@ const reducer = (state = 0, action) => {
   }
 };
 
-
 // store는 data를 저장하는 곳
 // CreateStore는 reducer를 요구함.
 const store = createStore(reducer);
 
+const onChange = () => {
+  number.innerHTML = store.getState();
+}
+
+// subscribe : store 안에 있는 변화 감지
+// store.subscribe(func); => store안의 변화를 감지하면 func 실행
+store.subscribe(onChange);
+
 // reducer에게 Action을 보내는 방법 : store.dispatch({key: value});
-store.dispatch({ type: "ADD" });
-store.dispatch({ type: "ADD" });
-store.dispatch({ type: "ADD" });
-store.dispatch({ type: "ADD" });
-store.dispatch({ type: "ADD" });
-store.dispatch({ type: "MINUS" });
-
-
-console.log(store.getState());
+plus.addEventListener("click", () => store.dispatch({ type: "PLUS"}));
+minus.addEventListener("click", () => store.dispatch({ type: "MINUS"}));
